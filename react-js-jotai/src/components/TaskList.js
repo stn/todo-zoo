becb468d4e781e264704c38663972c083b6fb3a5
Akira Ishino <akira.ishino@gmail.com>
2023-09-27 21:20:33 +0900

    Jotai

---
+++ react-js-jotai/src/components/TaskList.js
@@ -0,0 +1,67 @@
+import React, {useEffect, useRef, useState} from 'react';
+
+import FilterButton from './FilterButton';
+import TaskListHeading from './TaskListHeading';
+import Todo from './Todo';
+import {usePrevious} from '../hooks';
+import {useTasks} from '../TasksContext';
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+function TaskList(props) {
+  const tasks = useTasks();
+  const [filter, setFilter] = useState('All');
+
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+      />
+    ));
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+
+  const listHeadingRef = useRef(null);
+  const prevTaskLength = usePrevious(tasks.length);
+
+  useEffect(() => {
+    if (tasks.length - prevTaskLength === -1) {
+      listHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);
+
+  return (
+    <div>
+      <div className="filters btn-group stack-exception">{filterList}</div>
+      <div tabIndex="-1" ref={listHeadingRef}>
+        <TaskListHeading taskList={taskList} />
+      </div>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
+    </div>
+  );
+}
+
+export default TaskList;
