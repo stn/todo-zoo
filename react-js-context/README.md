d6fc3cf854e336c277a193ad9aee5bbe9715e8f3
Akira Ishino <akira.ishino@gmail.com>
2023-09-27 16:05:26 +0900

    Fork react-js-context from react-js-lifting-state

---
+++ react-js-context/README.md
@@ -0,0 +1,6 @@
+# ReactによるTodo
+
+react-js-state-liftingをベースにContext APIを用いたものに修正。
+
+参考: https://react.dev/learn/passing-data-deeply-with-context
+
---
+++ react-js-context/src/App.js
@@ -0,0 +1,59 @@
+import React, {useState} from 'react';
+import {nanoid} from 'nanoid';
+
+import Form from './components/Form';
+import TaskList from './components/TaskList';
+
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+
+  function addTask(name) {
+    const newTask = {id: `todo-${nanoid()}`, name, completed: false};
+    setTasks([...tasks, newTask]);
+  }
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return {...task, completed: !task.completed};
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        //
+        return {...task, name: newName};
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+  return (
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask}/>
+      <TaskList
+        tasks={tasks}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    </div>
+  );
+}
+
+export default App;
