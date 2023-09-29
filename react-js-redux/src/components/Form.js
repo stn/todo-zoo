5e8591728f8db51a11d3f01c771dd2dc12de0c43
Akira Ishino <akira.ishino@gmail.com>
2023-09-29 09:52:18 +0900

    Fork react-js-redux from react-js-reducer-context

---
+++ react-js-redux/src/components/Form.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+import {useTasksDispatch} from "../TasksContext";
+
+function Form(props) {
+  const dispatch = useTasksDispatch();
+  const [name, setName] = useState('');
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (name === '') {
+      return;
+    }
+    dispatch({
+      type: 'add_task',
+      name: name,
+    });
+    setName('');
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2 className="label-wrapper">
+        <label htmlFor="new-todo-input" className="label__lg">
+          What needs to be done?
+        </label>
+      </h2>
+      <input
+        type="text"
+        id="new-todo-input"
+        className="input input__lg"
+        name="text"
+        autoComplete="off"
+        value={name}
+        onChange={handleChange}
+      />
+      <button type="submit" className="btn btn__primary btn__lg">
+        Add
+      </button>
+    </form>
+  );
+}
+
+export default Form;
