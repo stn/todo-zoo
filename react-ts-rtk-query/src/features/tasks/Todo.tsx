79a34f413dbbcc58080c42cdb89f048a96457dd6
Akira Ishino <akira.ishino@gmail.com>
2023-09-30 11:44:45 +0900

    Start react-ts-rtk-query

---
+++ react-ts-rtk-query/src/features/tasks/Todo.tsx
@@ -0,0 +1,121 @@
+import React, {
+  ChangeEvent,
+  FormEvent,
+  useEffect,
+  useRef,
+  useState,
+} from "react"
+import { useDispatch } from "react-redux"
+
+import { usePrevious } from "../../app/hooks"
+import { editTask, deleteTask, toggleTaskCompleted } from "./tasksSlice"
+
+interface TodoProps {
+  id: string
+  name: string
+  completed: boolean
+}
+
+export default function Todo({ id, name, completed }: TodoProps) {
+  const dispatch = useDispatch()
+
+  const [isEditing, setEditing] = useState(false)
+  const [newName, setNewName] = useState("")
+
+  const wasEditing = usePrevious(isEditing)
+
+  const editFieldRef = useRef<HTMLInputElement>(null)
+  const editButtonRef = useRef<HTMLButtonElement>(null)
+
+  useEffect(() => {
+    if (!wasEditing && isEditing) {
+      editFieldRef.current?.focus()
+    }
+    if (wasEditing && !isEditing) {
+      editButtonRef.current?.focus()
+    }
+  }, [wasEditing, isEditing])
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setNewName(e.target.value)
+  }
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault()
+    dispatch(
+      editTask({
+        id,
+        name: newName,
+      }),
+    )
+    setNewName("")
+    setEditing(false)
+  }
+
+  const editingTemplate = (
+    <form className="stack-small" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label className="todo-label" htmlFor={id}>
+          New name for {name}
+        </label>
+        <input
+          id={id}
+          className="todo-text"
+          type="text"
+          value={newName}
+          onChange={handleChange}
+          ref={editFieldRef}
+        />
+      </div>
+      <div className="btn-group">
+        <button
+          type="button"
+          className="btn todo-cancel"
+          onClick={() => setEditing(false)}
+        >
+          Cancel
+          <span className="visually-hidden">renaming {name}</span>
+        </button>
+        <button type="submit" className="btn btn__primary todo-edit">
+          Save
+          <span className="visually-hidden">new name for {name}</span>
+        </button>
+      </div>
+    </form>
+  )
+
+  const viewTemplate = (
+    <div className="stack-small">
+      <div className="c-cb">
+        <input
+          id={id}
+          type="checkbox"
+          defaultChecked={completed}
+          onChange={() => dispatch(toggleTaskCompleted(id))}
+        />
+        <label className="todo-label" htmlFor={id}>
+          {name}
+        </label>
+      </div>
+      <div className="btn-group">
+        <button
+          type="button"
+          className="btn"
+          onClick={() => setEditing(true)}
+          ref={editButtonRef}
+        >
+          Edit <span className="visually-hidden">{name}</span>
+        </button>
+        <button
+          type="button"
+          className="btn btn__danger"
+          onClick={() => dispatch(deleteTask(id))}
+        >
+          Delete <span className="visually-hidden">{name}</span>
+        </button>
+      </div>
+    </div>
+  )
+
+  return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>
+}
