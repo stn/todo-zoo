6ff4ede47bbc47afd542dac3401b7969d504a9f5
Akira Ishino <akira.ishino@gmail.com>
2023-10-04 08:57:45 +0900

    Remove unused imports

--- server/djangorestframework/todo/todoapp/views.py
+++ server/djangorestframework/todo/todoapp/views.py
@@ -1,10 +1,7 @@
-from rest_framework.decorators import action
-from django.contrib.auth.models import User
-from rest_framework import permissions, viewsets
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-from rest_framework import renderers
 
 
 from .models import Todo
@@ -22,8 +19,6 @@ class TodoViewSet(viewsets.ModelViewSet):
     """
     This viewset automatically provides `list`, `create`, `retrieve`,
     `update` and `destroy` actions.
-
-    Additionally we also provide an extra `highlight` action.
     """
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
